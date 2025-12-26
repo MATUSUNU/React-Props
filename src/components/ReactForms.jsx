514643fe31b303974bf9d50a74a4795d36ba60ca
MatyaSina <matyassinaadugna@gmail.com>
2025-12-26 15:38:01 +0300

    part 2 try complex state

--- src/components/ReactForms.jsx
+++ src/components/ReactForms.jsx
@@ -2,46 +2,66 @@ import { useState } from "react";
 
 
 function ReactForms() {
-  const [fname, setFname] = useState("");
-  const [lname, setLname] = useState("");
-  const [heading, setHeading] = useState("");
+  // -------------------------------------- Part 1 --------------------------------------
+  // const [fname, setFname] = useState("");
+  // const [lname, setLname] = useState("");
+  // const [heading, setHeading] = useState("");
 
-  function handleFirstChange(event) {
-    // console.log(event.target.value);
-    // console.log(event.target.placeholder);
-    // console.log(event.target.type);
-    setFname(event.target.value);
-  }
+  // function handleFirstChange(event) {
+  //   // console.log(event.target.value);
+  //   // console.log(event.target.placeholder);
+  //   // console.log(event.target.type);
+  //   setFname(event.target.value);
+  // }
 
-  function handleSecondChange(event) {
-    setLname(event.target.value)
-  }
+  // function handleSecondChange(event) {
+  //   setLname(event.target.value)
+  // }
 
-  function handleClick(event) {
-    setHeading(fname+" "+lname);
+  // function handleClick(event) {
+  //   setHeading(fname+" "+lname);
 
-    event.preventDefault();
+  //   event.preventDefault();
+  // }
+
+  // -------------------------------------- Part 2 --------------------------------------
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  })
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const name = event.target.name;
+
+    if (name === "fname") {
+      setFullName({fName: newValue});
+    } else if (name === "lname") {
+      setFullName({lName: newValue});
+    }
   }
 
   return (
     <div className="container d-flex justify-content-center align-items-center mt-3">
       <div className="form-card p-4 text-center">
-        <h1 className="mb-4">Hello {fname} {lname}</h1>
+        <h1 className="mb-4">Hello {fullName.fName} {fullName.lName}</h1>
         <input
-          onChange={handleFirstChange}
+          onChange={handleChange}
           type="text"
+          name="fname"
           className="form-control mb-3"
           placeholder="What's your first name?"
-          value={fname}
+          value={fullName.fName}
         />
         <input
-          onChange={handleSecondChange}
+          onChange={handleChange}
           type="text"
+          name="lname"
           className="form-control mb-3"
           placeholder="What's your last name?"
-          value={lname}
+          value={fullName.lName}
         />
-        <button onClick={handleClick} className="btn btn-primary w-100">Submit</button>
+        <button className="btn btn-primary w-100">Submit</button>
       </div>
     </div>
   )
